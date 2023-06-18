442fb34ac2f7d7fb825ce916a368198b45b983fa
hiriski <hiriski@outlook.com>
2023-06-18 16:47:18 +0700

    install packages

--- index.js
+++ index.js
@@ -3,7 +3,8 @@
  */
 
 import { AppRegistry } from 'react-native'
-import App from './src/app'
+import App from './src/app.tsx'
 import { name as appName } from './src/app.json'
 
+import 'react-native-gesture-handler'
 AppRegistry.registerComponent(appName, () => App)
